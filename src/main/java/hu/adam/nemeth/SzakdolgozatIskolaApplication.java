8018347d37b691a9fbce798317c2b56d2823670a
Adam Nemeth <nemethadam88@gmail.com>
2019-03-12 10:52:41 +0100

    first commit

---
+++ src/main/java/hu/adam/nemeth/SzakdolgozatIskolaApplication.java
@@ -0,0 +1,13 @@
+package hu.adam.nemeth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzakdolgozatIskolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzakdolgozatIskolaApplication.class, args);
+	}
+
+}
