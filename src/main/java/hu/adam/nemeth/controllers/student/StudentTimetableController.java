1efbdf85123d24e42ba1cde8c75a757c0a6f6efe
Adam Nemeth <nemethadam88@gmail.com>
2019-03-29 10:56:20 +0100

    student timetable controller fixed

--- src/main/java/hu/adam/nemeth/controllers/student/StudentTimetableController.java
+++ src/main/java/hu/adam/nemeth/controllers/student/StudentTimetableController.java
@@ -10,10 +10,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.Comparator;
@@ -32,24 +29,19 @@ public class StudentTimetableController {
     MarkService markService;
     TeacherService teacherService;
 
-    @RequestMapping({"/timetable", "/timetable.html"})
-    public String timeTableWithoutParameter() {
-        return "redirect:/student/timetable?show=today";
-    }
-
-    @RequestMapping(value = "/timetable", params = "show")
-    public String timetable(Model model, @AuthenticationPrincipal UserDetails user, @RequestParam("show") String show) {
+    @GetMapping(value = "/timetable")
+    public String timetable(Model model, @AuthenticationPrincipal UserDetails user, @RequestParam(value = "show", required = false) String show) {
         Student student = studentService.findByUserName(user.getUsername());
-        List<Course> courses = student.getCourses().stream().sorted(Comparator.comparing(Course::getStartTime).reversed()).collect(Collectors.toList());
+        List<Course> courses = student.getCourses().stream()
+                .sorted(Comparator.comparing(Course::getStartTime).reversed())
+                .collect(Collectors.toList());
+
         Filter filter = new Filter();
 
-        if (show.equals("today")) {
-            courses = courseService.filterCoursesByDay(courses, LocalDate.now().toString());
-            filter.setDay(LocalDate.now().toString());
-        } else if (show.equals("all")) {
-
+        if (show != null) {
         } else {
-            return "redirect:/student/timetable";
+            filter.setDay(LocalDate.now().toString());
+            courses = courseService.filterCoursesByDay(courses, LocalDate.now().toString());
         }
 
         model.addAttribute("user", student);
@@ -60,7 +52,7 @@ public class StudentTimetableController {
         return "student/timetable";
     }
 
-    @PostMapping(value = "/timetable", params = "show")
+    @PostMapping(value = "/timetable")
     public String filterTimeTable(Model model, @ModelAttribute Filter filter, @AuthenticationPrincipal UserDetails user) {
         Student student = studentService.findByUserName(user.getUsername());
         List<Course> courses = student.getCourses().stream().sorted(Comparator.comparing(Course::getStartTime).reversed()).collect(Collectors.toList());
@@ -83,7 +75,6 @@ public class StudentTimetableController {
     class Filter {
         private String day;
         private String teacherId;
-
         public Filter() {
             this.day = "-1";
         }
